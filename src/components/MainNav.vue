6bbd4cb187879eee5c0eb4094d0a949dacb4b698
Hassan <hboulhilt@gmail.com>
2023-03-18 23:32:04 +0100

    Derictives part one

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,8 +1,26 @@
 <template>
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
-      <div class="border-brand-gray-1 mx-auto flex h-full flex-nowrap border-b border-solid px-8">
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+      <div
+        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
+      >
+        <a :href="url" class="flex h-full items-center text-xl">{{
+          company
+        }}</a>
+
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="ml-9 h-full first:ml-0"
+            >
+              <a href="" class="flex h-full items-center py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -10,11 +28,20 @@
 
 <script>
 export default {
-  name: 'MainNav',
+  name: "MainNav",
   data() {
     return {
-      company: 'Bobo Careers'
-    }
-  }
-}
+      company: "Bobo Careers",
+      url: "https://careers.google.com",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at Bobo Corp",
+        "How we hire",
+        "Students",
+        "Jobs",
+      ],
+    };
+  },
+};
 </script>
